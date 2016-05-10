234235627b57d9e2ca195092d150f93d4a445288
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2016-05-10 15:08:58 +0530

    ghost activity project added

---
+++ ghost_starter/app/src/main/java/com/google/engedu/ghost/GhostActivity.java
@@ -0,0 +1,75 @@
+package com.google.engedu.ghost;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class GhostActivity extends ActionBarActivity {
+    private static final String COMPUTER_TURN = "Computer's turn";
+    private static final String USER_TURN = "Your turn";
+    private GhostDictionary dictionary;
+    private boolean userTurn = false;
+    private Random random = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ghost);
+        onStart(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_ghost, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void computerTurn() {
+        TextView label = (TextView) findViewById(R.id.gameStatus);
+        // Do computer turn stuff then make it the user's turn again
+        userTurn = true;
+        label.setText(USER_TURN);
+    }
+
+    /**
+     * Handler for the "Reset" button.
+     * Randomly determines whether the game starts with a user turn or a computer turn.
+     * @param view
+     * @return true
+     */
+    public boolean onStart(View view) {
+        userTurn = random.nextBoolean();
+        TextView text = (TextView) findViewById(R.id.ghostText);
+        text.setText("");
+        TextView label = (TextView) findViewById(R.id.gameStatus);
+        if (userTurn) {
+            label.setText(USER_TURN);
+        } else {
+            label.setText(COMPUTER_TURN);
+            computerTurn();
+        }
+        return true;
+    }
+}
